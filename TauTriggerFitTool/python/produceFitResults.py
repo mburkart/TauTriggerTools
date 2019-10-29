a9c731df10323f0fdcb582c6cc5dd0d9396e4e20
Maximilian Burkart <maxiburkart@gmail.com>
2019-10-29 11:13:02 +0100

    Add configuration options to fit steering.

--- TauTriggerFitTool/python/produceFitResults.py
+++ TauTriggerFitTool/python/produceFitResults.py
@@ -1,40 +1,53 @@
-from ROOT import *
 from array import array
-gROOT.SetBatch(True)
+import argparse
 from math import sqrt
+
+import ROOT as root
+
+root.PyConfig.IgnoreCommandLineOptions = True
+root.gROOT.SetBatch(True)
+
 from functions import *
 from fittingTool import fittingTool
 
-year2017 = False
-year2018 = True
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", help="Input file")
+parser.add_argument("-o", "--output", help="Output file")
+parser.add_argument("-t", "--triggers", nargs="*", default=["etau", "mutau", "ditau"],
+                    help="Trigger to be considered.")
+parser.add_argument("-w", "--working-points", nargs="*",
+                    default=["vloose", "loose", "medium", "tight", "vtight", "vvtight"],
+                    help="Working points to be considered")
+parser.add_argument("-d", "--data", nargs="*", default=["DATA", "MC", "EMB"])
+parser.add_argument("--mva", action="store_true",
+                    help="Use MVA tau id instead of DeepTau ID.")
+args = parser.parse_args()
 
-if(year2017):
-	filename = "../data/tauTriggerEfficiencies2017_final_etauESshift_test.root"
-	file = TFile.Open(filename, "r")
-elif(year2018):
-	filename = "../data/tauTriggerEfficiencies2018.root"
-	file = TFile.Open(filename, "r")
+
+# filename = "../data/output_2017_tau_leg_earlyFilters.root"
+filename = args.input
+file = root.TFile.Open(filename, "r")
 
 
-triggers = ["ditau", "mutau", "etau"]
-types = ["DATA", "MC"]
-WPs = ["vvlooseTauMVA","vlooseTauMVA", "looseTauMVA", "mediumTauMVA", "tightTauMVA", "vtightTauMVA", "vvtightTauMVA"]
-tauDMs = ["dm0", "dm1", "dm10"]
+triggers = args.triggers
+types = args.data
+if args.mva:
+    WPs = [wp + "TauMVA" for wp in args.working_points]
+    tauDMs = ["dm0", "dm1", "dm10"]
+else:
+    WPs = [wp + "DeepTau" for wp in args.working_points]
+    tauDMs = ["dm0", "dm1", "dm10", "dm11"]
 
 isDMspesific = True
 
-gStyle.SetFrameLineWidth(1)
-gStyle.SetPadBottomMargin(0.13)
-gStyle.SetPadLeftMargin(0.15)
-gStyle.SetPadTopMargin(0.09)
-gStyle.SetPadRightMargin(0.05)
+root.gStyle.SetFrameLineWidth(1)
+root.gStyle.SetPadBottomMargin(0.13)
+root.gStyle.SetPadLeftMargin(0.15)
+root.gStyle.SetPadTopMargin(0.09)
+root.gStyle.SetPadRightMargin(0.05)
 
-if(year2017):
-	outputname = "tauTriggerFitResults_2017_test.root"
-elif(year2018):
-	outputname = "tauTriggerFitResults_2018_test.root"
-
-outputfile = TFile( "../data/"+outputname, 'recreate')
+# outputfile = TFile( "../data/"+outputname, 'recreate')
+outputfile = root.TFile(args.output, 'recreate')
 
 nfailed = 0
 # efficiency calculation after filling the histograms for 3 different triggers for each WPs of DATA and MC
@@ -48,16 +61,16 @@ for ipath, trigger in enumerate(triggers):
 			g_errBandDM68.append([])
 			for index, typ in enumerate(types):
 				histoname = "histo_" + typ + "_" + wp + "_" + trigger
-				h_errBandDM68[idm].append(TH1F(histoname+ "_" + DM +"_CL68","histo of 0.68 confidence band", 480, 20, 500))
-				g_errBandDM68[idm].append(TGraphErrors())
+				h_errBandDM68[idm].append(root.TH1F(histoname+ "_" + DM +"_CL68","histo of 0.68 confidence band", 480, 20, 500))
+				g_errBandDM68[idm].append(root.TGraphErrors())
 		
 		for index, typ in enumerate(types):
 			
-			f1.append(TF1( 'f1'+typ, '[5] - ROOT::Math::crystalball_cdf(-x, [0], [1], [2], [3])*([4])'))
+			f1.append(root.TF1( 'f1'+typ, '[5] - ROOT::Math::crystalball_cdf(-x, [0], [1], [2], [3])*([4])'))
 			if(index ==0):
-				f1[index].SetLineColor( kBlue)
+				f1[index].SetLineColor( root.kBlue)
 			else:
-				f1[index].SetLineColor( kRed)
+				f1[index].SetLineColor( root.kRed)
 			f1[index].SetParName( 0, "alpha" )
 			f1[index].SetParName( 1, "n" )
 			f1[index].SetParName( 2, "sigma" )
@@ -69,13 +82,13 @@ for ipath, trigger in enumerate(triggers):
                 for idm, DM in enumerate(tauDMs):
                         f2.append([])
                         for index, typ in enumerate(types):
-                                f2[idm].append(TF1( 'f2_'+ DM  +"_" + typ, '[5] - ROOT::Math::crystalball_cdf(-x, [0], [1], [2], [3])*([4])'))
-                                if(idm ==0): f2[idm][index].SetLineColor( kBlue )
-                                elif(idm ==1): f2[idm][index].SetLineColor( kRed )
-                                elif(idm ==2): f2[idm][index].SetLineColor( kGreen+3 )
+                                f2[idm].append(root.TF1( 'f2_'+ DM  +"_" + typ, '[5] - ROOT::Math::crystalball_cdf(-x, [0], [1], [2], [3])*([4])'))
+                                if(idm ==0): f2[idm][index].SetLineColor( root.kBlue )
+                                elif(idm ==1): f2[idm][index].SetLineColor( root.kRed )
+                                elif(idm ==2): f2[idm][index].SetLineColor( root.kGreen+3 )
                                 if( isDMspesific):
-                                        if index==0: f2[idm][0].SetLineColor( kBlue )
-                                        elif index==1: f2[idm][1].SetLineColor( kRed )
+                                        if index==0: f2[idm][0].SetLineColor( root.kBlue )
+                                        elif index==1: f2[idm][1].SetLineColor( root.kRed )
                                 f2[idm][index].SetParName( 0, "alpha" )
                                 f2[idm][index].SetParName( 1, "n" )
                                 f2[idm][index].SetParName( 2, "sigma" )
@@ -90,15 +103,20 @@ for ipath, trigger in enumerate(triggers):
 	
 			print "Fit is performed for", trigger, "trigger in", wp ,"WP for", typ
 			
-			gEfficiency = TGraphAsymmErrors()
-			gEfficiency = file.Get(trigger +"_gEfficiency_" + wp +"_"+ typ)
+			gEfficiency = root.TGraphAsymmErrors()
+                        query = "graphv2_" + trigger +"TriggerEfficiency_" + wp +"_"+ typ
+                        print "query: ", query
+			gEfficiency = file.Get(query)
+                        if isinstance(gEfficiency, root.TObject) and not isinstance(gEfficiency, root.RooHist):
+                            print "Histogram {} not found in file {}".format(query, args.input)
+                            raise Exception
 
 			fitter = fittingTool(f1[index], gEfficiency)
 
 			print "trigger", trigger
 
-			h_errBand68.append(TH1F(histoname+"_CL68","histo of 0.68 confidence band", 480, 20, 500))
-			g_errBand68.append(TGraphErrors())
+			h_errBand68.append(root.TH1F(histoname+"_CL68","histo of 0.68 confidence band", 480, 20, 500))
+			g_errBand68.append(root.TGraphErrors())
 			
 			fit_result = fitter.performFit()
 			if int(fit_result) != 0: nfailed += 1
@@ -125,8 +143,8 @@ for ipath, trigger in enumerate(triggers):
 			g_errBand68[index].Write(trigger + "_gerrBand_"+ wp +"_"+ typ)
 	
 			#======== Relative error of the fit: "fit +/- error/ fit " ===================
-			relativeErrorUP = TGraphAsymmErrors()
-			relativeErrorDown = TGraphAsymmErrors()
+			relativeErrorUP = root.TGraphAsymmErrors()
+			relativeErrorDown = root.TGraphAsymmErrors()
 			relativeErrorUP, relativeErrorDown = funct.createRelativeErrors()
 
 			relativeErrorUP.Write(trigger  +"_relativeErrorUp_" + wp + "_"+ typ )
@@ -136,8 +154,13 @@ for ipath, trigger in enumerate(triggers):
 			for idm, DM in enumerate(tauDMs):
 				print "Fit is performed for", trigger, "trigger in", wp ,"WP for", typ , " per DM", DM
 				
-				gEfficiencyDM = TGraphAsymmErrors()
-				gEfficiencyDM = file.Get(trigger +"_gEfficiency_" + wp +"_"+ typ +"_"+ DM)
+				gEfficiencyDM = root.TGraphAsymmErrors()
+                                query = "graphv2_" + trigger + "TriggerEfficiency_" + wp +"_"+ DM +"_"+ typ
+                                print "query: ", query
+				gEfficiencyDM = file.Get(query)
+                                if isinstance(gEfficiency, root.TObject) and not isinstance(gEfficiency, root.RooHist):
+                                    print "Histogram {} not found in file {}".format(query, args.input)
+                                    raise Exception
 				
 				fitter = fittingTool(f2[idm][index], gEfficiencyDM)
 
@@ -148,7 +171,7 @@ for ipath, trigger in enumerate(triggers):
 				functDM = functions(gEfficiency, "histo_" + trigger + "_" + wp +"_"+ typ, idm, index, f2[idm] , h_errBandDM68[idm][index], g_errBandDM68[idm][index], fit_result2, 0.68)
 				h_errBandDM68[idm][index], g_errBandDM68[idm][index] = functDM.getConfidenceInterval()
 
-				f2result = TF1( 'f2', '[5] - ROOT::Math::crystalball_cdf(-x, [0], [1], [2], [3])*([4])', 0, 500)
+				f2result = root.TF1( 'f2', '[5] - ROOT::Math::crystalball_cdf(-x, [0], [1], [2], [3])*([4])', 0, 500)
 				f2result.SetParameter(0, f2[idm][index].GetParameter(0))
 				f2result.SetParameter(1, f2[idm][index].GetParameter(1))
 				f2result.SetParameter(2, f2[idm][index].GetParameter(2))
@@ -164,8 +187,8 @@ for ipath, trigger in enumerate(triggers):
 
 
 		# Getting Scale Factors
-		SF = TGraphAsymmErrors()
-		SFerror = TH1F()
+		SF = root.TGraphAsymmErrors()
+		SFerror = root.TH1F()
 		functSF = functions(gEfficiency, "histo_" + trigger + "ErrorBand_" + wp +"_"+ typ, idm, index, f1, h_errBand68, g_errBand68, fit_result, 0.68) 
 			
 		SFerror = functSF.getScaleFactorError()
@@ -175,8 +198,8 @@ for ipath, trigger in enumerate(triggers):
 		
 		# Getting Scale Factors per decay mode
 		for idm, DM in enumerate(tauDMs):
-			SFdm = TGraphErrors()
-			SFdmerror = TH1F()
+			SFdm = root.TGraphErrors()
+			SFdmerror = root.TH1F()
 			functDMSF = functions(gEfficiency, "histo_" + trigger + "ErrorBand_" + wp +"_"+ typ, idm, index, f2[idm], h_errBandDM68[idm], g_errBandDM68[idm], fit_result2, 0.68) 
 			
 			SFdmerror = functDMSF.getScaleFactorError()
@@ -187,4 +210,4 @@ for ipath, trigger in enumerate(triggers):
 
 outputfile.Close()
 print nfailed, 'failed fits'
-print "The output ROOT file has been created: ../data/" + outputname
+print "The output ROOT file has been created: " + outputfile.GetName()
